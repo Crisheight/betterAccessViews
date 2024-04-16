329620ca52ded2b0bda8519a48e1c926ea287fc5
Cris <crisheight@gmail.com>
2024-04-15 22:09:49 -0500

    Wiped JS for now, will update to listen for onclick() in future.

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link, .nav__linkalt');
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-});
