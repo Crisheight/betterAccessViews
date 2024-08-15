d85d171ddc2bea2ef5f6623e656656f52df2b2cf
Cris <crisheight@gmail.com>
2024-08-15 17:02:19 -0500

    Added Maps Places Logic, removed previous searchBar logic.

--- main.js
+++ main.js
@@ -1,32 +1,69 @@
-const searchIcon = document.getElementById('searchIcon');
-const locationSearchBar = document.getElementById('locationSearchBar');
-
 /* ************************
- *  Geolocation API Logic *
+ *  Google Maps Logic     *
  * ************************  */
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-        (position) => {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-            locationSearchBar.value = `${latitude}, ${longitude}`;
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 40.749933, lng: -73.98633 },
+        zoom: 13,
+        mapTypeControl: false,
+    });
+    const card = document.getElementById("pac-card");
+    const input = document.getElementById("pac-input");
+    const biasInputElement = document.getElementById("use-location-bias");
+    const strictBoundsInputElement = document.getElementById("use-strict-bounds");
+    const options = {
+        fields: ["formatted_address", "geometry", "name"],
+        strictBounds: false,
+    };
 
-        },
-        (error) => {
-            console.error("Error getting location:", error);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
+
+    const autocomplete = new google.maps.places.Autocomplete(input, options);
+
+    // Bind the map's bounds (viewport) property to the autocomplete object,
+    // so that the autocomplete requests use the current map bounds for the
+    // bounds option in the request.
+    autocomplete.bindTo("bounds", map);
+
+    const infowindow = new google.maps.InfoWindow();
+    const infowindowContent = document.getElementById("infowindow-content");
+
+    infowindow.setContent(infowindowContent);
+
+    const marker = new google.maps.Marker({
+        map,
+        anchorPoint: new google.maps.Point(0, -29),
+    });
+
+    autocomplete.addListener("place_changed", () => {
+        infowindow.close();
+        marker.setVisible(false);
+
+        const place = autocomplete.getPlace();
+
+        if (!place.geometry || !place.geometry.location) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
         }
-    );
-} else {
-    console.error("Geolocation is not supported by this browser.");
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+        }
+
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+        infowindowContent.children["place-name"].textContent = place.name;
+        infowindowContent.children["place-address"].textContent =
+            place.formatted_address;
+        infowindow.open(map, marker);
+    });
 }
 
-/* ************************
- *  Search Icon Logic     *
- * ************************  */
-
-searchIcon.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.location.href = 'stage2.html';
-});
+window.initMap = initMap;
