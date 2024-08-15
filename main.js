71645a00db68ef9474b04ec2c75fb98374b06cf8
Cris <crisheight@gmail.com>
2024-08-14 19:06:31 -0500

    Added logic for geolocation info

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 const searchIcon = document.getElementById('searchIcon');
+const locationSearchBar = document.getElementById('locationSearchBar');
+
+/* ************************
+ *  Geolocation API Logic *
+ * ************************  */
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+            locationSearchBar.value = `${latitude}, ${longitude}`;
+
+        },
+        (error) => {
+            console.error("Error getting location:", error);
+        }
+    );
+} else {
+    console.error("Geolocation is not supported by this browser.");
+}
+
+/* ************************
+ *  Search Icon Logic     *
+ * ************************  */
 
 searchIcon.addEventListener('click', (e) => {
     e.preventDefault();
     window.location.href = 'stage2.html';
-    console.log("Hello");
 });
